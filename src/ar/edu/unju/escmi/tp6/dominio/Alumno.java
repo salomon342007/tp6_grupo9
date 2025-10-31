ed35b6ff1d405ae9cadc8d8e4fa49b7e3232ef71
Colturas <Pachekin8989@gmail.com>
2025-10-31 20:34:22 -0300

    Cambios pedidos en el diagrama

--- src/ar/edu/unju/escmi/tp6/dominio/Alumno.java
+++ src/ar/edu/unju/escmi/tp6/dominio/Alumno.java
@@ -1,25 +1,31 @@
 package ar.edu.unju.escmi.tp6.dominio;
 
 public class Alumno extends Usuario {
+    // Nº de libreta del alumno
     private String nroLibreta;
+    // Curso o división del alumno
     private String curso;
 
+    // Constructor: inicializa usuario y campos específicos de Alumno
     public Alumno(int id, String nombre, String apellido, String email, String nroLibreta, String curso) {
         super(id, nombre, apellido, email);
         this.nroLibreta = nroLibreta;
         this.curso = curso;
     }
 
+    // Devuelve el número de libreta
     public String getNroLibreta() {
         return nroLibreta;
     }
 
+    // Devuelve el curso
     public String getCurso() {
         return curso;
     }
 
     @Override
     public void mostrarDatos() {
+        // Imprime por consola los datos del alumno
         System.out.println("---- Alumno ----");
         System.out.println("ID: " + id);
         System.out.println("Nombre: " + nombre + " " + apellido);
